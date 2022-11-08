69a6133e70074572c1dabe675174265c1a1e7831
HarivigneshDevarajan <92356268+HarivigneshDevarajan@users.noreply.github.com>
2022-11-08 11:33:38 +0530

    Initial commit

---
+++ src/app/product-alerts/product-alerts.component.ts
@@ -0,0 +1,18 @@
+import { Component, Output, Input, EventEmitter } from '@angular/core';
+import { Product } from '../products';
+
+@Component({
+  selector: 'app-product-alerts',
+  templateUrl: './product-alerts.component.html',
+  styleUrls: ['./product-alerts.component.css']
+})
+export class ProductAlertsComponent {
+
+  constructor() { }
+
+  @Input() product: Product | undefined;
+  @Output() notify = new EventEmitter();
+
+   
+
+}
